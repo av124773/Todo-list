94f1df9b3d6b23c7e7117afec5b0ea692d985fc5
av124773 <av124773@gmail.com>
2023-02-14 14:12:05 +0800

    feat: add mongoose and dotenv

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// 僅在非正式環境時，使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3001
+
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
 
 app.get('/', (req,res) => {
   res.send('hello')
