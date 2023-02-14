10e1eb1116896b164e47e63b28b3aadc1d66ca25
av124773 <av124773@gmail.com>
2023-02-14 15:49:00 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 const port = 3001
 
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+//取得資料庫連現狀態
+const db = mongoose.connection
+// 連現異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongoose connected!')
+})
 
 app.get('/', (req,res) => {
   res.send('hello')
