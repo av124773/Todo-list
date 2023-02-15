df7eb453edff85e8d254ff88e68e3d77516ad33b
av124773 <av124773@gmail.com>
2023-02-15 10:32:26 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const todo = require('./models/todo')
 const exphbs = require('express-handlebars')
-
-// 僅在非正式環境時，使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const Todo = require('./models/todo')
 
 const app = express()
 const port = 3001
 
 app.engine('hbs', exphbs({ default: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+// 僅在非正式環境時，使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
@@ -28,7 +28,10 @@ db.once('open', () => {
 })
 
 app.get('/', (req,res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣版
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.listen(port, () => {
