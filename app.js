d3670627886bd8cb61b28c723d294604141b2cd4
av124773 <av124773@gmail.com>
2023-02-20 17:38:47 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const routes = require('./routes')
 
-const Todo = require('./models/todo')
-const { SchemaTypeOptions } = require('mongoose')
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3001
 
-// 僅在非正式環境時，使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-//取得資料庫連現狀態
-const db = mongoose.connection
-// 連現異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongoose connected!')
-})
-
 app.engine('hbs', exphbs({ default: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
 app.use(routes)
